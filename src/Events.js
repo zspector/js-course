17f9aa18464d4681cc28672ac59db39e32cd3fbd
Zachary Spector <zjspector@gmail.com>
2014-05-30 10:21:24 -0500

    Write Extend function and Event object so all tests pass

--- src/Events.js
+++ src/Events.js
@@ -1,3 +1,19 @@
 var Events = {
+  on: function(event, func) {
+    if (this.events == undefined) {
+      this.events = {};
+    }
+    if (this.events[event] == undefined) {
+      this.events[event] = [];
+      this.events[event].push(func);
+    } else {
+      this.events[event].push(func);
+    }
+  },
 
+  trigger: function(event) {
+    for (i = 0; i < this.events[event].length; i++) {
+      this.events[event][i]();
+    }
+  }
 };
--- src/Extend.js
+++ src/Extend.js
@@ -1,2 +1,6 @@
-var extend = function() {
+var extend = function(destination, source) {
+  for (key in source) {
+    destination[key] = source[key];
+  }
+  return destination;
 };
