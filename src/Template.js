914d4e4d36d0f5a4167208ee350158dfea851d3e
Zachary Spector <zjspector@gmail.com>
2014-05-30 11:01:30 -0500

    Pass tests for templates branch

--- src/Template.js
+++ src/Template.js
@@ -1,3 +1,13 @@
-var render = function(){
-
+var render = function(template, data){
+  // find values in template to be interpolated
+  // save values in array
+  // match saved values with data object properties
+  // if they match, interpolate
+  // remove double curly braces
+  var renderedTemplate = template;
+  for (var property in data) {
+    var regex = new RegExp("{{" + property + "}}", "g");
+    var renderedTemplate = renderedTemplate.replace( regex, data[property]);
+  }
+  return renderedTemplate;
 };
